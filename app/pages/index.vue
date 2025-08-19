3c43cd127f797932b7f1e21e9440fa49f7d655b6
Mondotosz <git@moncorp.ch>
2025-08-19 14:04:45 +0200

    copied from frontend for the landing page

---
+++ app/pages/index.vue
@@ -0,0 +1,10 @@
+<!--
+  NOTE: The landing page must use static content since it will be prerendered.
+  Any dynamic page should be wrapped in `<ClientOnly></ClientOnly>`
+-->
+<template>
+  <div>
+    <h1>Landing page</h1>
+    <NuxtLink to="/dashboard">Dashboard</NuxtLink>
+  </div>
+</template>
